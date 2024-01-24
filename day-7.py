48ba25655d74459b6240c2c8224438bc38caa30c
agcdtmr <112581827+agcdtmr@users.noreply.github.com>
2024-01-24 14:02:15 +0100

    Create day-7.py

---
+++ day-7.py
@@ -0,0 +1,41 @@
+# Challenge: Write a program to check if a number is positive, negative, or zero.
+
+# PROCESS ON SOLVING CODING CHALLENGES
+
+# 1. Understand the Problem
+# Inputs:
+# Outputs:
+# Restrictions:
+
+# 2. Examples
+
+# Example 1:
+# Input:
+# Output:
+
+# Example 2:
+# Input:
+# Output:
+
+# 3. Pseudocode
+
+# 4. Solve/Simplify
+
+# 5. Refactor or Look Back
+
+# - Time Complexity
+# - Space Complexity (Memory Usage)
+# - Code Readability and Maintainability
+# - Error Handling
+# - Optimizations
+# - Testing: unit testing and pytest
+# - Documentation: README file with instructions on how to use your code
+# - Use version control systems like Git to track changes to your code and collaborate with others
+
+# 6. Test the code
+
+# Optional: Document errors you encountered
+
+# 7. Double Check the Documentation
+
+# 8. Submit the Solution
