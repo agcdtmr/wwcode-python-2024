6aceba1e93804ec5bbdd0579bc0fc9b980123d03
agcdtmr <anjdeitmer@gmail.com>
2024-02-16 11:54:54 +0100

    Update day-12.py

--- day-12.py
+++ day-12.py
@@ -1,1 +1,15 @@
 # Challenge: Write a program to reverse a given string. If you want, take it a step further and use recursion.
+
+
+given_string = input("Type a string you want to reverse: ")
+
+
+def reverse_string(given_string):
+    reversed_string = ""
+    for char in given_string:
+        reversed_string = char + reversed_string
+    return reversed_string
+
+
+result = reverse_string(given_string)
+print(result)
