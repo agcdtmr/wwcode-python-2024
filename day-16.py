a60ce699a6c6efd3693d88243e0ab8a2ed542eab
adtmr <anjdeitmer@gmail.com>
2024-03-13 08:36:02 +0100

    Create day-16.py

---
+++ day-16.py
@@ -0,0 +1,13 @@
+
+# # Day 16
+
+# :jigsaw: Python Days of Code Challenge - Day 16 :jigsaw:
+# :bulb: How It Works:
+# :calendar: 60 Days, 60 Questions: Dive into daily Python challenges, each marking a step closer to coding mastery. You can start anytime in between as well!
+# :handshake: Learning Together:
+# Share your solutions, ask questions, and connect with fellow coders in this thread or on social media (Twitter, LinkedIn) - or Github!
+# Don't forget to tag us @WWCodePython or use the hashtags #WomenWhoCodePython #PythonDaysofCode so we can celebrate your achievements!
+# :gift: Bonus:
+# React to today’s question if you’d like us to solve this LIVE in our upcoming Python Code Jam Sessions
+# :brain: Today's Challenge:
+# Write a function that counts the frequency of each word in a sentence.
