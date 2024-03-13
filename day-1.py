118ac00d6d76f42c733f3cafd6eb0958a5c2034b
adtmr <anjdeitmer@gmail.com>
2024-03-13 08:35:45 +0100

    Update day-1.py

--- day-1.py
+++ day-1.py
@@ -1,4 +1,4 @@
-## Challenge: Create a program that swaps the values of two variables.
+# Challenge: Create a program that swaps the values of two variables.
 
 print("                                             ")
 variableOne = input("Type your first random variable: ")
@@ -10,8 +10,7 @@ print("First random variable: ", variableOne)
 print("Second random variable: ", variableTwo)
 
 
-
-## Swappin'
+# Swappin'
 
 temporaryHolder = variableOne
 variableOne = variableTwo
@@ -22,3 +21,36 @@ print("These are your random variables AFTER swapping")
 print("First random variable: ", variableOne)
 print("Second random variable: ", variableTwo)
 print("                                             ")
+
+
+# # Day 1
+
+# ```
+# Python Days of Code Challenge - Day 1
+
+# How It Works:
+# - 60 Days, 60 Questions: Dive into daily Python challenges, each marking a step closer to coding mastery. You can start anytime in between as well!
+
+# Learning Together:
+# - [ ] Share your solutions, ask questions, and connect with fellow coders in this thread or on social media (Twitter, LinkedIn) - or Github!
+# - [ ] Don't forget to tag us @WWCodePython or use the hashtags #WomenWhoCodePython #PythonDaysofCode so we can celebrate your achievements!
+
+# Bonus:
+# - [ ] React to today’s question if you’d like us to solve this LIVE in our upcoming Python Code Jam Sessions
+
+# Today's Challenge:
+# - [ ] Create a program that swaps the values of two variables.
+# ```
+
+# ### Daily goals:
+# - [x] Code [Day 1](https://github.com/agcdtmr/wwcode-python-2024/blob/main/day-1.py)
+# - [x] Write an awesome commit message and push your code!
+# - [x] Continue writing on your documentation.
+# - [x] Share your wins!
+# - [x] Help someone on Slack. Give feedback, share resources, be a rubber ducky.
+# ![day1help](https://github.com/agcdtmr/wwcode-python-2024/blob/main/day1-help-p.png)
+# - [x] Take care of yourself: pause, stretch, hydrate. More on [WWCode Code of Balance](https://www.womenwhocode.com/blog/category/mental-health).
+
+
+# ### Weekly goals
+# - [x] Send appreciation message to a WWCode volunteer or member. See the photo above.
