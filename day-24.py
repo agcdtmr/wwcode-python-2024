40b16ee2007b65939cb540a3b841598cd8f28c0b
adtmr <anjdeitmer@gmail.com>
2024-03-13 08:36:23 +0100

    Create day-24.py

---
+++ day-24.py
@@ -0,0 +1,16 @@
+
+
+# # Day 24
+
+# :jigsaw: Python Days of Code Challenge - Day 24 :jigsaw:
+# :bulb: How It Works:
+# :calendar: 60 Days, 60 Questions: Dive into daily Python challenges, each marking a step closer to coding mastery. You can start anytime in between as well!
+# :handshake: Learning Together:
+# Share
+#  your solutions, ask questions, and connect with fellow coders in this
+# thread or on social media (Twitter, LinkedIn) - or Github!
+# Don't forget to tag us @WWCodePython or use the hashtags #WomenWhoCodePython #PythonDaysofCode so we can celebrate your achievements!
+# :gift: Bonus:
+# React to today’s question with a ":question:" if you’d like us to solve this LIVE in our upcoming Python Code Jam Sessions.
+# :brain: Today's Challenge:
+# Write a program to remove vowels from a given string.
