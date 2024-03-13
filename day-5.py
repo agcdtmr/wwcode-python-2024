312b7923ac05d8ec3ca64047bcabcfcb879a037e
adtmr <anjdeitmer@gmail.com>
2024-03-13 08:36:30 +0100

    Update day-5.py

--- day-5.py
+++ day-5.py
@@ -46,28 +46,52 @@ maximum_value = max(list1)
 print(f"The maximum value is: {maximum_value}")
 
 
-
 # 5. Refactor or Look Back
 
 # With more knowledge, I would like to refactor this to where I can combine str and int in one list
 
 # 6. Test the code
 
-# # Error1 python day-5.py 
+# # Error1 python day-5.py
 # File "/workspaces/wwcode-python-2024/day-5.py", line 24
 #     list1 = [0, 1, 2, 4, 5, 7, 8, 9, 100, 900, a, $, ^]
 #                                                   ^
 # SyntaxError: invalid syntax
 
-# # Error2 python day-5.py 
+# # Error2 python day-5.py
 #   File "/workspaces/wwcode-python-2024/day-5.py", line 24, in <module>
 #     list1 = [0, 1, 2, 4, 5, 7, 8, 9, 100, 900, a, Z]
 # NameError: name 'a' is not defined
 
-# # Error3 python day-5.py 
+# # Error3 python day-5.py
 #   File "/workspaces/wwcode-python-2024/day-5.py", line 26, in <module>
 #     minimum_value = min(list1)
 # TypeError: '<' not supported between instances of 'str' and 'int'
 
 
+# # Day 5
 
+# ```
+# Python Days of Code Challenge - Day 5
+
+# How It Works:
+# - 60 Days, 60 Questions: Dive into daily Python challenges, each marking a step closer to coding mastery. You can start anytime in between as well!
+
+# Learning Together:
+# - [ ] Share your solutions, ask questions, and connect with fellow coders in this thread or on social media (Twitter, LinkedIn) - or Github!
+# - [ ] Don't forget to tag us @WWCodePython or use the hashtags #WomenWhoCodePython #PythonDaysofCode so we can celebrate your achievements!
+
+# Bonus:
+# - [ ] React to today’s question if you’d like us to solve this LIVE in our upcoming Python Code Jam Sessions
+
+# Today's Challenge:
+# - [ ] Write a program to find the maximum and minimum values in a list.
+# ```
+
+# ### Daily goals
+
+# - [x] Code [Day 5](https://github.com/agcdtmr/wwcode-python-2024/blob/main/day-5.py)
+# - [x] Write an awesome commit message and push your code!
+# - [x] Continue writing on your documentation.
+# - [x] Share your wins!
+# - [x] Take care of yourself: pause, stretch, hydrate. More on [WWCode Code of Balance](https://www.womenwhocode.com/blog/category/mental-health).
